00f4d21bedd0d7a4595db7fdbee857e7900189b1
janebv15 <jvaughn0@chicagobooth.edu>
2025-04-21 14:51:43 +0000

    sync

--- app.rb
+++ app.rb
@@ -1,6 +1,38 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+#square
+get "/square/results" do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get "/square_root/new" do
+  erb(:square_root_calc)
+end
+
+#square root
+get "/square_root/results" do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+  erb(:square_root_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
 require "./config/environment"
+require "sinatra"
 run(Sinatra::Application)
